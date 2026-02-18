3fff8ac8855f40e1c7b8cf66c64aca95b01b23c0
Michael Piko <michael@piko.com.au>
2026-02-18 14:35:11 +1100

    remove geo as not use

--- cs_client_equipment/__manifest__.py
+++ cs_client_equipment/__manifest__.py
@@ -12,7 +12,7 @@
 
 {
     'name': 'Client Equipment',
-    'version': '19.0.1.0.5',
+    'version': '19.0.1.0.6',
     'category': 'Productivity',
     'author': 'Cyder Solutions',
     'website': 'https://www.cyder.com.au',
